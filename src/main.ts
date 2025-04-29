ea28f438e9ec94881241cf787e3ac35a231de44d
joel alwan sembiring <joel.2381038@unai.edu>
2025-04-29 21:22:28 +0700

    last please last

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,51 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors();
 
-  // Setup Swagger config
   const config = new DocumentBuilder()
-    .setTitle('Pemesanan Jasa Buruh API')
-    .setDescription('API untuk pemesanan jasa buruh pertanian')
-    .setVersion('1.0')
-    .addBearerAuth() // Untuk autentikasi Bearer JWT
+    .setTitle('Pemesanan jasa buruh API')
+    .addBearerAuth()
+    .addSecurityRequirements('bearer')
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
+  // Serve raw OpenAPI JSON
+  app.use('/api/swagger-json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(document);
+  });
 
-  // Swagger UI akan bisa diakses di /api/swagger
-  SwaggerModule.setup('api/swagger', app, document);
+  // Serve Swagger UI HTML (from CDN)
+  app.use('/api/swagger', (req, res) => {
+    res.send(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>API Docs</title>
+          <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
+        </head>
+        <body>
+          <div id="swagger-ui"></div>
+          <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+          <script>
+            window.onload = function () {
+              SwaggerUIBundle({
+                url: '/api/swagger-json',
+                dom_id: '#swagger-ui',
+              });
+            };
+          </script>
+        </body>
+      </html>
+    `);
+  });
 
-  // Gunakan PORT dari environment untuk deployment (contoh: Render/Railway)
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-
-  console.log(`Application is running on: http://localhost:${port}/api/swagger`);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT ?? 3000);
 }
-
 bootstrap();
